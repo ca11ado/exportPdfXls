f4dd5292849f42d4500cf128dd26a5807781b1d0
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-10 20:34:52 +0600

    set content-type header in middleware receive params from url

--- app/app.js
+++ app/app.js
@@ -9,12 +9,10 @@ let app = express();
 
 let outputMessage = 'Root>>>\n';
 
-app.use('/api', exportMiddleware);
+app.use('/api/:org', exportMiddleware);
 
 app.get('*', (req, res) => {
   outputMessage += 'Get>>>>';
-  //res.set('Content-Type', 'application/pdf');
-  //res.type('application/pdf');
   res.send(outputMessage);
 
   //clear
@@ -25,10 +23,9 @@ app.listen(3000);
 
 function exportMiddleware (req, res, next) {
   outputMessage += 'Test Middleware>>>\n';
-  console.log(Object.keys(res));
-  console.log(Object.keys(res._headers));
+
+  console.log(req.params.org);
+
   res.set('Content-Type', 'application/pdf');
-  res.set('My-Test', 't0s');
-  console.log(Object.keys(res._headers));
   res.send('Here is my middleware');
 }
