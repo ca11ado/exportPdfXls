754d77ff73ad0c685c8735350a966a137729c372
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-11 13:54:44 +0300

    пример со стримингом pdf в ответ от сервера
     app.get('/pdf')...

--- app/app.js
+++ app/app.js
@@ -4,13 +4,26 @@
 'use strict';
 require('babel-register');
 
+let PDFDocument = require('pdfkit');
+let doc = new PDFDocument();
+
 let exportMiddleware = require('./export-middleware');
 let express = require('express');
 let app = express();
 
 let outputMessage = 'Root>>>\n';
+let stream;
 
 app.use('/api/:org', exportMiddleware);
+
+app.get('/pdf', (req, res) => {
+  stream = doc.pipe(res);
+  doc.text('Hello');
+  doc.end();
+  stream.on('finish', () => {
+    res.send();
+  });
+});
 
 app.get('*', (req, res) => {
   outputMessage += 'Get>>>>';
@@ -19,5 +32,6 @@ app.get('*', (req, res) => {
   //clear
   outputMessage = 'Root>>>';
 });
+
 
 app.listen(3000);
