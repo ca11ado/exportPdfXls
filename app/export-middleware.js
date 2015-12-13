2a822040df58875a5737b0f1473593509506380d
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-13 08:47:07 +0300

    test xlsx sends to user

--- app/export-middleware.js
+++ app/export-middleware.js
@@ -1,7 +1,10 @@
 'use strict';
 
+let fs = require('fs');
+let path = require('path');
 let getAsyncData = require('./getAsyncData');
 let statisticaPdf = require('./pdf/statistic');
+let xlsxTemplate = require('xlsx-template');
 let stream;
 
 function exportMiddleware (req, res, next) {
@@ -22,7 +25,25 @@ function exportMiddleware (req, res, next) {
         });
       break;
     case 'xls':
-      res.send('xls');
+      fs.readFile(path.join(__dirname, 'xls/templates', 'simple3-template.xlsx'), function(err, data) {
+        var t = new xlsxTemplate(data);
+        t.substitute("Tables", {
+          ages: [
+            {name: "John", age: 10},
+            {name: "Bill", age: 12}
+          ],
+          days: ["Monday", "Tuesday", "Wednesday"],
+          hours: [
+            {name: "Bob", days: [10, 20, 30]},
+            {name: "Jim", days: [12, 24, 36]}
+          ],
+          progress: 100
+        });
+
+        var newData = t.generate();
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8');
+        res.send(new Buffer(newData, 'binary'));
+      });
       break;
     default:
       next();
